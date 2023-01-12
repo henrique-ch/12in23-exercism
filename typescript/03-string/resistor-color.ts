28916e61e4c27c52c04ad89aec32193e6c656fa3
Carlos Henrique <50886743+henrique-ch@users.noreply.github.com>
2023-01-12 19:42:06 -0300

    Update resistor-color.ts

--- typescript/03-string/resistor-color.ts
+++ typescript/03-string/resistor-color.ts
@@ -1,1 +1,17 @@
 
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white" 
+]
+
+export const colorCode = (value: string) => {
+  return COLORS.indexOf(value)
+}
